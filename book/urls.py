a66ec0fc5e812fa43177c575d0f9267c3d6dde8c
Anai0318 <s2322073@stu.musashino-u.ac.jp>
2023-10-11 05:14:06 +0900

    大枠完成

--- book/urls.py
+++ book/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('detail/<int:pk>/', views.Detail.as_view(), name='detail'),
     path('create/', views.Create.as_view(), name='create'),
     path('update/<int:pk>/', views.Update.as_view(), name='update'),
-    path('delete/<int:pk>/', views.Delete.as_view(), name='delete')
+    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
+    path('user_list/<int:user>/', views.UserView.as_view(), name='user_list'),
+    path('mypage/', views.Mypage.as_view(), name='mypage'),
+
+  
+    
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -7,11 +9,12 @@ from .forms import BookForm
 
 class List(generic.ListView):
     model = Book
-    paginate_by = 3
-
+    paginate_by = 4
 
 class Detail(generic.DetailView):
     model = Book
+    def get_queryset(self):
+        return Book.objects.all().order_by('-posted_date')
 
 class Create(generic.CreateView):
     model = Book
@@ -35,4 +38,25 @@ class Delete(generic.DeleteView):
     model = Book
     success_url = reverse_lazy('book:list')
 
-# Create your views here.
+
+class UserView(generic.ListView):
+    
+    template_name = 'book/book_list.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user_id = self.kwargs['user']
+        user_list = Book.objects.filter(user=user_id).order_by('-posted_date')
+
+        return user_list
+    
+
+class Mypage(generic.ListView):
+
+    template_name = 'book/mypage.html'
+    paginate_by = 1
+
+    def get_queryset(self):
+        querySet = Book.objects.filter(user=self.request.user).order_by('-posted_date')
+
+        return querySet
